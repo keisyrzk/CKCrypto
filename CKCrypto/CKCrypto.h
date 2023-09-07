dbd773f95e590fe814133b26b4220ea454c6c60d
Selfcode <krzysztof.banaczyk@callstack.com>
2023-09-07 10:23:08 +0200

    Initial Commit

---
+++ CKCrypto/CKCrypto.h
@@ -0,0 +1,18 @@
+//
+//  CKCrypto.h
+//  CKCrypto
+//
+//  Created by Krzysztof Banaczyk on 07/09/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CKCrypto.
+FOUNDATION_EXPORT double CKCryptoVersionNumber;
+
+//! Project version string for CKCrypto.
+FOUNDATION_EXPORT const unsigned char CKCryptoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CKCrypto/PublicHeader.h>
+
+
